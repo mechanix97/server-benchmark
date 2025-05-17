9b0c79f6a14a28590378414ffaf7cd684ec13db8
Gonzalo Puy <30372906+PuyGonzalo@users.noreply.github.com>
2025-05-16 23:15:02 -0300

    Update server.ex

--- elixir/lib/server.ex
+++ elixir/lib/server.ex
@@ -1,16 +1,47 @@
 defmodule JsonRpcServer do
   require Logger
+  use GenServer
 
-  def start do
-    {:ok, socket} = :gen_tcp.listen(8080, [:binary, packet: :http_bin, active: false, reuseaddr: true])
-    Logger.info("Server started on port 8080")
-    accept_loop(socket)
+  def start_link(_opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
-  defp accept_loop(socket) do
-    {:ok, client} = :gen_tcp.accept(socket)
-    serve(client)
-    accept_loop(socket)
+  def init(:ok) do
+    {:ok, socket} = :gen_tcp.listen(8080, [
+      :binary,
+      packet: :http_bin,
+      active: true,
+      reuseaddr: true,
+      nodelay: true
+    ])
+    Logger.info("Server started on port 8080")
+    {:ok, _} = Task.Supervisor.start_link(name: JsonRpcServer.TaskSupervisor)
+    send(self(), :accept)
+    {:ok, %{socket: socket}}
+  end
+
+  def handle_info(:accept, state = %{socket: socket}) do
+    case :gen_tcp.accept(socket) do
+      {:ok, client} ->
+        {:ok, _pid} = Task.Supervisor.start_child(
+          JsonRpcServer.TaskSupervisor,
+          fn -> serve(client) end
+        )
+        send(self(), :accept)
+        {:noreply, state}
+      {:error, reason} ->
+        Logger.error("Accept failed: #{inspect(reason)}")
+        {:stop, reason, state}
+    end
+  end
+
+  def handle_info({:tcp, client, packet}, state) do
+    # Handle TCP messages if needed for active mode
+    {:noreply, state}
+  end
+
+  def handle_info({:tcp_closed, _client}, state) do
+    {:noreply, state}
   end
 
   defp serve(client) do
@@ -18,25 +49,45 @@ defmodule JsonRpcServer do
       {:ok, body} ->
         case Jason.decode(body) do
           {:ok, _json} ->
-            :gen_tcp.send(client, "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n{\"status\":\"success\"}\r\n")
+            send_response(client, 200, %{"status" => "success"})
           {:error, _} ->
-            :gen_tcp.send(client, "HTTP/1.1 400 Bad Request\r\nContent-Type: application/json\r\n\r\n{\"status\":\"error\"}\r\n")
+            send_response(client, 400, %{"status" => "error"})
         end
       _ ->
-        :gen_tcp.send(client, "HTTP/1.1 400 Bad Request\r\nContent-Type: application/json\r\n\r\n{\"status\":\"error\"}\r\n")
+        send_response(client, 400, %{"status" => "error"})
     end
     :gen_tcp.close(client)
   end
 
   defp read_request(client, acc) do
-    case :gen_tcp.recv(client, 0) do
-      {:ok, :http_eoh} -> {:ok, Enum.join(acc, "")}
-      {:ok, {:http_request, :POST, _, _}} -> read_request(client, acc)
-      {:ok, {:http_header, _, _, _, _}} -> read_request(client, acc)
-      {:ok, data} -> read_request(client, [data | acc])
-      {:error, _} -> :error
+    receive do
+      {:tcp, ^client, :http_eoh} ->
+        {:ok, Enum.join(acc, "")}
+      {:tcp, ^client, {:http_request, :POST, _, _}} ->
+        read_request(client, acc)
+      {:tcp, ^client, {:http_header, _, _, _, _}} ->
+        read_request(client, acc)
+      {:tcp, ^client, data} ->
+        read_request(client, [data | acc])
+      {:tcp_closed, ^client} ->
+        :error
+    after
+      5_000 -> :error
     end
   end
-end
 
-JsonRpcServer.start()
+  defp send_response(client, status_code, body) do
+    response = Jason.encode!(body)
+    headers = [
+      "HTTP/1.1 #{status_code} #{status_text(status_code)}\r\n",
+      "Content-Type: application/json\r\n",
+      "Content-Length: #{byte_size(response)}\r\n",
+      "\r\n",
+      response
+    ]
+    :gen_tcp.send(client, headers)
+  end
+
+  defp status_text(200), do: "OK"
+  defp status_text(400), do: "Bad Request"
+end
